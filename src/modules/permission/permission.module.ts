bb4f829d921bb37551d57237dc60041d6213308c
watcharathep <watcharathep@knstrans.com>
2020-08-30 18:47:24 +0700

    import controller and service

--- src/modules/permission/permission.module.ts
+++ src/modules/permission/permission.module.ts
@@ -4,10 +4,12 @@ import { GroupRepository } from './repositories/group.repository';
 import { MenuService } from './services/menu.service';
 import { MenuController } from './controllers/menu.controller';
 import { MenuRepository } from './repositories/menu.repository';
+import { GroupController } from './controllers/group.controller';
+import { GroupService } from './services/group.service';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([GroupRepository,MenuRepository])],
-  providers: [MenuService],
-  controllers: [MenuController]
+  imports: [TypeOrmModule.forFeature([GroupRepository, MenuRepository])],
+  providers: [MenuService, GroupService],
+  controllers: [MenuController, GroupController],
 })
 export class PermissionModule {}
